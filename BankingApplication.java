e14d55327b4bc3cce6df5d8e9bbe73439675d5b9
PriyankaShejul <priyankashejul493@gmail.com>
2025-09-15 13:50:47 +0530

    Add files via upload

---
+++ BankingApplication.java
@@ -0,0 +1,12 @@
+package JavaProjects;
+
+public class BankingApplication {
+
+	public static void main(String[] args) {
+		BankAccount obj = new BankAccount("Priyanka","P1234");
+		obj.showMenu();
+		// TODO Auto-generated method stub
+
+	}
+
+}
